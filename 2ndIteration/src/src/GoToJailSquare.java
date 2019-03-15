442c9e6d3e2464557dcf5a2be90ec1e7bb2db83a
erhan7410 <erhngngr1995@gmail.com>
2019-03-15 15:50:51 +0300

    Full Project

---
+++ 2ndIteration/src/src/GoToJailSquare.java
@@ -0,0 +1,12 @@
+public class GoToJailSquare extends Square{
+
+	protected GoToJailSquare(int squareLocation,String name) {
+		super(squareLocation,name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void goToJail(Player p) {
+		p.getPlayerPiece().setLocation(10);
+		p.setJail(true);
+	}		
+}
---
+++ 2ndIteration/src/src/Money.java
@@ -0,0 +1,21 @@
+public class Money {
+	private int money;
+	private String type = "$";
+			
+	protected Money(int money) {
+		this.money = money;
+	}
+	
+	public int getMoney() {
+		return this.money;
+	}
+	
+	public void setMoney(int money) {
+		this.money += money;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+}
